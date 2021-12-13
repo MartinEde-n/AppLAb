0685539d0848a565e71ff2bf75c25547b34e1eed
MartinEde-n <evgen.vezstuden@gmail.com>
2021-12-13 04:07:45 +0300

    added price

---
+++ app/js/main.js
@@ -0,0 +1,18 @@
+
+
+
+$(function(){
+  $('.satisfaction__slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows:true,
+		dots:false,
+  });
+	
+	// $('.icon-th-list').on('click', function(){
+	// 	$('.products__item').addClass('list plus')
+	// 	$('.icon-th-list').addClass('active ')
+	// 	$('.icon-th-large').removeClass('active ')
+	// });
+});
+
