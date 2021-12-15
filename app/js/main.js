883a1b292d923dbccc95ae03956955647ae7005b
MartinEde-n <evgen.vezstuden@gmail.com>
2021-12-15 17:22:15 +0300

    Added project

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,7 @@
-
-
-
 $(function(){
-  $('.satisfaction__slider').slick({
+  $('.client__inner-box').slick({
 		slidesToShow: 1,
 		slidesToScroll: 1,
-		arrows:true,
-		dots:false,
   });
-	
-	// $('.icon-th-list').on('click', function(){
-	// 	$('.products__item').addClass('list plus')
-	// 	$('.icon-th-list').addClass('active ')
-	// 	$('.icon-th-large').removeClass('active ')
-	// });
 });
 
